2d1f0f4962964045571e94d2131077a262205f31
vedenij <3633077@gmail.com>
2025-12-31 14:06:30 -0800

    last

--- start_server.sh
+++ start_server.sh
@@ -6,4 +6,5 @@
 mkdir -p /var/log/model
 
 # Start model server and log to file (PyWorker monitors this log)
-python model_server.py 2>&1 | tee /var/log/model/server.log
+# Use stdbuf for line-buffered output so PyWorker sees logs immediately
+stdbuf -oL python model_server.py 2>&1 | stdbuf -oL tee /var/log/model/server.log
